9975434b4388b235148506bfc3019bc62a37afa4
◪ DARK LOVER <116174408+dark-lover-opz@users.noreply.github.com>
2022-10-21 22:33:48 +0530

    This is my 2nd pr

--- console.js
+++ console.js
@@ -3,3 +3,9 @@ console.log ( " Hello World " )
 console.log ( " Hello World " ) // 2nd time!
 console.log ( " Hello World " ) // 3nd time!
    function = log ("hello world")
+         
+
+console.log ( " Hello World " )
+console.log ( " Hello World " ) // 2nd time!
+console.log ( " Hello World " ) // 3nd time!
+   function = log ("hello world")
